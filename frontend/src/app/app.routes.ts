e2b75b4528eaca0f47426fe3a08de990e05ea69c
Fernanda Costa <fer-mcosta@hotmail.com>
2025-06-26 11:06:34 -0300

    feat: controle de acesso de rota

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,51 +1,79 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './modules/login/login.component';
-import { AuthGuard } from './auth/auth.guard';
-import { HomeMotoristaComponent } from './modules/motorista/home-motorista/home-motorista.component';
-import { HomeAdminComponent } from './modules/admin/home-admin/home-admin.component';
-import { GerenciarMotoristasComponent } from './modules/motorista/gerenciar-motoristas/gerenciar-motoristas.component';
-import { AgendarViagemComponent } from './modules/agendamento/agendar-viagem/agendar-viagem.component';
-import { ListaVeiculosComponent } from './modules/veiculo/lista-veiculos/lista-veiculos.component';
-import { ListaMotoristaComponent } from './modules/motorista/lista-motorista/lista-motorista.component';
-import { GerenciarVeiculosComponent } from './modules/veiculo/gerenciar-veiculos/gerenciar-veiculos.component';
-import { OcorrenciaListComponent } from './modules/ocorrencia/ocorrencia-list/ocorrencia-list.component';
+import { Routes } from "@angular/router";
+import { AuthGuard } from "./auth/auth.guard";
+import { HomeAdminComponent } from "./modules/admin/home-admin/home-admin.component";
+import { AgendarViagemComponent } from "./modules/agendamento/agendar-viagem/agendar-viagem.component";
+import { LoginComponent } from "./modules/login/login.component";
+import { GerenciarMotoristasComponent } from "./modules/motorista/gerenciar-motoristas/gerenciar-motoristas.component";
+import { HomeMotoristaComponent } from "./modules/motorista/home-motorista/home-motorista.component";
+import { ListaMotoristaComponent } from "./modules/motorista/lista-motorista/lista-motorista.component";
+import { OcorrenciaListComponent } from "./modules/ocorrencia/ocorrencia-list/ocorrencia-list.component";
+import { GerenciarVeiculosComponent } from "./modules/veiculo/gerenciar-veiculos/gerenciar-veiculos.component";
+import { ListaVeiculosComponent } from "./modules/veiculo/lista-veiculos/lista-veiculos.component";
 
 export const routes: Routes = [
-    {
-        path: 'login',
-        component: LoginComponent,
-        canActivate: [AuthGuard]
-    },
-    {
-        path: 'motorista',
-        component: HomeMotoristaComponent,
-        canActivate: [AuthGuard]
-    },
-    {
-        path: 'admin',
-        component: HomeAdminComponent,
-        canActivate: [AuthGuard]
-    },
-    {
-        path: 'ocorrencias',
-        component: OcorrenciaListComponent,
-        canActivate: [AuthGuard]
-    },
-    { path: 'motoristas', component: ListaMotoristaComponent },
-    { path: 'motoristas/cadastrar', component: GerenciarMotoristasComponent },
-    { path: 'motoristas/editar/:id', component: GerenciarMotoristasComponent },
-    {
-        path: 'veiculos',
-        component: ListaVeiculosComponent,
-        canActivate: [AuthGuard]
-    },
-    { path: 'veiculos/cadastrar', component: GerenciarVeiculosComponent },
-    { path: 'veiculos/editar/:id', component: GerenciarVeiculosComponent },
-
-    {
-        path: 'viagem',
-        component: AgendarViagemComponent,
-        canActivate: [AuthGuard]
-    },
-    { path: '', redirectTo: 'motorista', pathMatch: 'full' }
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'motorista',
+    component: HomeMotoristaComponent,
+    canActivate: [AuthGuard],
+    data: { perfis: ['MOTORISTA'] }
+  },
+  {
+    path: 'admin',
+    component: HomeAdminComponent,
+    canActivate: [AuthGuard],
+    data: { perfis: ['ADMINISTRADOR'] }
+  },
+  {
+    path: 'ocorrencias',
+    component: OcorrenciaListComponent,
+    canActivate: [AuthGuard],
+    data: { perfis: ['ADMINISTRADOR'] }
+  },
+  {
+    path: 'motoristas',
+    component: ListaMotoristaComponent,
+    canActivate: [AuthGuard],
+    data: { perfis: ['ADMINISTRADOR'] }
+  },
+  {
+    path: 'motoristas/cadastrar',
+    component: GerenciarMotoristasComponent,
+    canActivate: [AuthGuard],
+    data: { perfis: ['ADMINISTRADOR'] }
+  },
+  {
+    path: 'motoristas/editar/:id',
+    component: GerenciarMotoristasComponent,
+    canActivate: [AuthGuard],
+    data: { perfis: ['ADMINISTRADOR'] }
+  },
+  {
+    path: 'veiculos',
+    component: ListaVeiculosComponent,
+    canActivate: [AuthGuard],
+    data: { perfis: ['ADMINISTRADOR'] }
+  },
+  {
+    path: 'veiculos/cadastrar',
+    component: GerenciarVeiculosComponent,
+    canActivate: [AuthGuard],
+    data: { perfis: ['ADMINISTRADOR'] }
+  },
+  {
+    path: 'veiculos/editar/:id',
+    component: GerenciarVeiculosComponent,
+    canActivate: [AuthGuard],
+    data: { perfis: ['ADMINISTRADOR'] }
+  },
+  {
+    path: 'viagem',
+    component: AgendarViagemComponent,
+    canActivate: [AuthGuard],
+    data: { perfis: ['ADMINISTRADOR'] }
+  },
+  { path: '', redirectTo: 'motorista', pathMatch: 'full' }
 ];
--- frontend/src/app/auth/auth.guard.ts
+++ frontend/src/app/auth/auth.guard.ts
@@ -1,4 +1,3 @@
-// src/app/core/guards/auth.guard.ts
 import { Injectable } from '@angular/core';
 import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from '../services/auth.service';
@@ -8,27 +7,30 @@ export class AuthGuard implements CanActivate {
   constructor(private auth: AuthService, private router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-
     const estaAutenticado = this.auth.estaAutenticado;
     const rotaDestino = route.routeConfig?.path;
+
     if (rotaDestino === 'login' && estaAutenticado) {
-
-      if (this.auth.perfilUsuario == 'ADMINISTRADOR') {
+      if (this.auth.perfilUsuario === 'ADMINISTRADOR') {
         this.router.navigate(['/admin']);
-        return false;
-      }
-
-      if (this.auth.perfilUsuario == 'MOTORISTA') {
+      } else if (this.auth.perfilUsuario === 'MOTORISTA') {
         this.router.navigate(['/motorista']);
-        return false;
       }
-
       return false;
-
     }
 
     if (!estaAutenticado && rotaDestino !== 'login') {
       this.router.navigate(['/login']);
+      return false;
+    }
+
+    const perfisPermitidos = route.data?.['perfis'];
+    if (perfisPermitidos && !perfisPermitidos.includes(this.auth.perfilUsuario)) {
+      if (this.auth.perfilUsuario === 'ADMINISTRADOR') {
+        this.router.navigate(['/admin']);
+      } else if (this.auth.perfilUsuario === 'MOTORISTA') {
+        this.router.navigate(['/motorista']);
+      }
       return false;
     }
 
